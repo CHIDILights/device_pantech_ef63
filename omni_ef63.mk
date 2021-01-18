caf3fb4543571459e6d32fe80a93318f584b5d3f
CHIDILights <tomysean@naver.com>
2021-01-18 21:15:50 +0900

    Lineage to Omni

--- omni_ef63.mk
+++ omni_ef63.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2018 The LineageOS Project
+# Copyright 2019 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,5 +14,15 @@
 # limitations under the License.
 #
 
- PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/omni_ef63.mk
+# Inherit from those products. Most specific first.
+$(call inherit-product, build/target/product/embedded.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := ef63
+PRODUCT_NAME := omni_ef63
+PRODUCT_BRAND := VEGA
+PRODUCT_MODEL := VEGA IRON2
+PRODUCT_MANUFACTURER := Pantech
