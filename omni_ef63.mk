0c063eac3c254b69024891092172347b3501f1a6
CHIDILights <tomysean@naver.com>
2021-01-19 12:58:25 +0900

    Add PRODUCT_COPY_FILES flag for fstab

--- omni_ef63.mk
+++ omni_ef63.mk
@@ -17,11 +17,14 @@
 # Release name
 PRODUCT_RELEASE_NAME := ef63
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, build/target/product/embedded.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/omni/config/common.mk)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/recovery.fstab:recovery/root/etc/twrp.fstab
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := ef63
