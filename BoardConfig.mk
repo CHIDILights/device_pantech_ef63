f0dfcf6a88f6f655da91e0f1bf29268279eef04c
CHIDILights <tomysean@naver.com>
2021-01-19 13:05:16 +0900

    Add BoardConfig.mk from https://github.com/lonelystar9x/device_pantech_ef63/blob/Android-8.1/BoardConfig.mk https://github.com/TeamWin/android_device_pantech_ef63/blob/android-8.1/BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -14,28 +14,64 @@
 # limitations under the License.
 #
 
-PLATFORM_PATH := device/pantech/ef63
-# Inherit from msm8974-common
--include device/pantech/msm8974-common/BoardConfigCommon.mk
+# Allow missing dependencies
+ALLOW_MISSING_DEPENDENCIES  := true
 
-# Assert
-TARGET_OTA_ASSERT_DEVICE := ef63l,ef63s,ef63k
+# Platform
+TARGET_BOARD_PLATFORM			 := msm8974
+TARGET_BOARD_PLATFORM_GPU  := qcom-adreno330
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_PATH)/bluetooth
+# Architecture
+TARGET_ARCH					 := arm
+TARGET_ARCH_VARIANT  := armv7-a-neon
+TARGET_CPU_ABI			 := armeabi-v7a
+TARGET_CPU_ABI2 		 := armeabi
+TARGET_CPU_VARIANT   := krait
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME  := MSM8974
+TARGET_NO_RADIOIMAGE				  := true
 
 # Kernel
-TARGET_KERNEL_CONFIG := LineageOS_ef63_defconfig
-
-# ReleaseTools
-TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)/releasetools
-
-# Properties
-TARGET_SYSTEM_PROP += $(PLATFORM_PATH)/system.prop
+BOARD_KERNEL_CMDLINE 		 := console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 vmalloc=260M loglevel=0 androidboot.selinux=permissive
+BOARD_KERNEL_BASE 			 := 0x0000000
+BOARD_KERNEL_PAGESIZE		 := 2048
+BOARD_MKBOOTIMG_ARGS  	 := --kernel_offset 0x0008000 --ramdisk_offset 0x2000000  --tags_offset 0x0000100 --dt device/pantech/ef63/prebuilt/dt.img
+TARGET_PREBUILT_KERNEL   := device/pantech/ef63/prebuilt/kernel
+BOARD_KERNEL_IMAGE_NAME  := kernel
 
 # Filesystem
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 26534215680
+TARGET_USERIMAGES_USE_EXT4  := true
+TARGET_USERIMAGES_USE_F2FS  := true
+BOARD_HAS_LARGE_FILESYSTEM  := true
 
-# Init
-TARGET_INIT_VENDOR_LIB := libinit_ef63
-TARGET_RECOVERY_DEVICE_MODULES := libinit_ef63
+# Partitions
+BOARD_SYSTEMIMAGE_PARTITION_SIZE 		:= 2566914048
+BOARD_BOOTIMAGE_PARTITION_SIZE 			:= 33554432
+BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 33554432
+BOARD_USERDATAIMAGE_PARTITION_SIZE  := 26534215680
+BOARD_FLASH_BLOCK_SIZE 							:= 131072
+BOARD_VOLD_EMMC_SHARES_DEV_MAJOR 	  := true
+
+# Debug
+TWRP_INCLUDE_LOGCAT  := true
+TARGET_USES_LOGD	   := true
+
+# TWRP
+TW_THEME 												  := portrait_hdpi
+TW_INPUT_BLACKLIST 							  := "fpc1080Mouse"
+TW_NO_USB_STORAGE								  := true
+TW_CRYPTO_FS_TYPE								  := "ext4"
+TW_DEFAULT_EXTERNAL_STORAGE			  := true
+TW_INCLUDE_JB_CRYPTO						  := true
+TW_INCLUDE_CRYPTO 							  := true
+TW_BRIGHTNESS_PATH    					  := "/sys/class/leds/lcd-backlight/brightness"
+TW_MAX_BRIGHTNESS 							  := 255
+TW_DEFAULT_BRIGHTNESS 	 					:= 175
+TW_EXTRA_LANGUAGES 							  := true
+TARGET_RECOVERY_PIXEL_FORMAT 		  := "RGBX_8888"
+TARGET_RECOVERY_QCOM_RTC_FIX 		  := true
+BOARD_SUPPRESS_SECURE_ERASE			  := true
+BOARD_SUPPRESS_EMMC_WIPE 				  := true
+RECOVERY_SDCARD_ON_DATA 				  := true
+RECOVERY_GRAPHICS_USE_LINELENGTH  := true
