7c150d064fa5953f8bb26e3a9acfbfc5bea0e841
CHIDILights <tomysean@naver.com>
2021-02-24 20:31:59 +0900

    Update device name

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -17,7 +17,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "VEGA IRON 2"
+#define BTM_DEF_LOCAL_NAME   "VEGA IRON2"
 
 // #define BLE_VND_INCLUDED          TRUE
 #define BLUETOOTH_QTI_SW          TRUE
